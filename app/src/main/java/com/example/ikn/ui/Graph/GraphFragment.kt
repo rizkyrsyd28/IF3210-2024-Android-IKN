488393b3de43b24f54268af8729ab1097b4784bb
SaddamAnnais <saddamannaiss@gmail.com>
2024-03-30 11:40:49 +0700

    fix: navigation to graph fix

--- app/src/main/java/com/example/ikn/ui/Graph/GraphFragment.kt
+++ app/src/main/java/com/example/ikn/ui/Graph/GraphFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ikn.ui.Graf
+package com.example.ikn.ui.Graph
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.ikn.R
 
-class GrafFragment : Fragment() {
+class GraphFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graf, container, false)
+        return inflater.inflate(R.layout.fragment_graph, container, false)
     }
 }
